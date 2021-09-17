7bea581f74a48d4bf684784d6b3286ac6f1df73e
Abdirahman Jama <abdirahman.jamaaa@gmail.com>
2021-09-17 02:56:10 +0100

    add buttons for light and darkmode

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-
+import { IconButton } from "@chakra-ui/button";
+import { Flex, Heading, VStack } from "@chakra-ui/layout";
+import {FaSun, FaMoon, FaInstagram, FaGithub, FaLinkedin} from 'react-icons/fa'
 
 function App() {
   return (
-    <div className="App">
-     Hello
-    </div>
+    //vstack allows us to have a column from top to vottom 
+    <VStack>
+      <Flex w="100%">
+        <Heading ml="8" size="md" fontweight='semibold' color='cyan.400'>abdirahman jama</Heading>
+      </Flex>
+
+      <IconButton icon = {<FaSun/>} isRound="true">
+
+      </IconButton>
+    </VStack> 
   );
 }
 
---
+++ src/components/Socials.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Socials() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Socials
